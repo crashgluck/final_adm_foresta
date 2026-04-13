b1a46d7b735c16bbad72756d79472e762abf652c
crashgluck <cris.vera.olivares@gmail.com>
2026-04-13 15:34:33 -0400

    preparacion de produccion

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,11 @@
 ﻿from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
+from .views import index_start
 
 urlpatterns = [
+    path('', index_start, name='index'),
+    re_path(r"^comunidad/.*$", index_start),
     path('admin/', admin.site.urls),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
