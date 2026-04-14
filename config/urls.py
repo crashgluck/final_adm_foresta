d740b82ce0663090db534440b76afaa942bd1695
crashgluck <cris.vera.olivares@gmail.com>
2026-04-14 11:28:20 -0400

    cambios para produccion

--- config/urls.py
+++ config/urls.py
@@ -1,15 +1,15 @@
-﻿from django.contrib import admin
+from django.contrib import admin
 from django.urls import include, path, re_path
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
-from .views import index_start
 
 urlpatterns = [
-    path('', index_start, name='index'),
-    re_path(r"^comunidad/.*$", index_start),
-    path('admin/', admin.site.urls),
-    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-    path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
-    path('api/v1/', include('apps.api.urls')),
+    path("admin/", admin.site.urls),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("api/docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
+    path("api/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
+    path("api/v1/", include("apps.api.urls")),
+    path("foresta/api/schema/", SpectacularAPIView.as_view(), name="foresta-schema"),
+    path("foresta/api/docs/", SpectacularSwaggerView.as_view(url_name="foresta-schema"), name="foresta-swagger-ui"),
+    path("foresta/api/redoc/", SpectacularRedocView.as_view(url_name="foresta-schema"), name="foresta-redoc"),
+    path("foresta/api/v1/", include("apps.api.urls")),
 ]
-
