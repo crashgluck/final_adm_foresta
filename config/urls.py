1204733c7efe1e63b82d5be2983fefe66b3c4a4f
crashgluck <cris.vera.olivares@gmail.com>
2026-04-14 12:16:49 -0400

    nueva

--- config/urls.py
+++ config/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import include, path, re_path
+from django.urls import include, path
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
 urlpatterns = [
@@ -8,8 +8,4 @@ urlpatterns = [
     path("api/docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
     path("api/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
     path("api/v1/", include("apps.api.urls")),
-    path("foresta/api/schema/", SpectacularAPIView.as_view(), name="foresta-schema"),
-    path("foresta/api/docs/", SpectacularSwaggerView.as_view(url_name="foresta-schema"), name="foresta-swagger-ui"),
-    path("foresta/api/redoc/", SpectacularRedocView.as_view(url_name="foresta-schema"), name="foresta-redoc"),
-    path("foresta/api/v1/", include("apps.api.urls")),
 ]
